eaadc20be20e6e62ff3211dd0e0e6cf714c629be
gabrielztk <38334108+gabrielztk@users.noreply.github.com>
2019-03-22 09:38:00 -0300

    BinaryDigit, Register8, Register16

--- Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
@@ -19,7 +19,33 @@ architecture rtl of BinaryDigit is
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
 
+	component FlipFlopD IS
+		port(
+			clock:  in std_logic;
+			d:      in std_logic;
+			clear:  in std_logic;
+			preset: in std_logic;
+			q:     out std_logic
+		);
+	end component;
+
+
+	component Mux2Way IS
+		port(
+			a:   in  STD_LOGIC;
+			b:   in  STD_LOGIC;
+			sel: in  STD_LOGIC;
+			q:   out STD_LOGIC
+		);
+	end component;
+
+	SIGNAL muxout, ffout : STD_LOGIC;	
+
 begin
+
+	Mux : Mux2Way port map (b => input, a => ffout, sel => load, q=> muxout);
+	FFp : FlipFlopD port map(clock => clock, d => muxout, clear => '0', preset => '0', q => ffout);
+	output <= ffout;
 
 
 end architecture;
