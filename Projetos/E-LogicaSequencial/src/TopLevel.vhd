dbfffb6b22870df91c665b085480bf9fd0e4147c
gabrielztk <38334108+gabrielztk@users.noreply.github.com>
2019-03-26 21:47:17 -0300

    Ram4K e TopLevel

--- Projetos/E-LogicaSequencial/src/TopLevel.vhd
+++ Projetos/E-LogicaSequencial/src/TopLevel.vhd
@@ -1,18 +1,9 @@
---
--- Elementos de Sistemas - Aula 5 - Logica Combinacional
--- Rafael . Corsi @ insper . edu . br 
---
--- Arquivo exemplo para acionar os LEDs e ler os bottoes
--- da placa DE0-CV utilizada no curso de elementos de 
--- sistemas do 3s da eng. da computacao
-
 ----------------------------
 -- Bibliotecas ieee       --
 ----------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.all;
 
 ----------------------------
 -- Entrada e saidas do bloco
@@ -21,6 +12,9 @@ entity TopLevel is
 	port(
 		SW      : in  std_logic_vector(9 downto 0);
 		KEY     : in  std_logic_vector(3 downto 0);
+		HEX0    : out std_logic_vector(6 downto 0); -- 7seg0
+		HEX1    : out std_logic_vector(6 downto 0); -- 7seg1
+		HEX2    : out std_logic_vector(6 downto 0); -- 7seg2
 		LEDR    : out std_logic_vector(9 downto 0)
 	);
 end entity;
@@ -30,40 +24,76 @@ end entity;
 ----------------------------
 architecture rtl of TopLevel is
 
-
-component FlipFlopD is
-	port(
-		clock:  in std_logic;
-		d:      in std_logic;
-		clear:  in std_logic;
-		preset: in std_logic;
-		q:     out std_logic
-	);
-end component;
-
 --------------
 -- signals
 --------------
+SIGNAL saida_display : std_logic_vector(15 downto 0);
+signal clock, load : std_logic;
 
-signal clock, clear, set : std_logic;
+--------------
+-- component
+--------------
+
+component Ram8 is
+	port(
+		clock:   in  STD_LOGIC;
+		input:   in  STD_LOGIC_VECTOR(15 downto 0);
+		load:    in  STD_LOGIC;
+		address: in  STD_LOGIC_VECTOR( 2 downto 0);
+		output:  out STD_LOGIC_VECTOR(15 downto 0)
+	);
+end component;
 
 ---------------
 -- implementacao
 ---------------
 begin
 
-Clock <= not KEY(0); -- os botoes quando nao apertado vale 1
-                     -- e apertado 0, essa logica inverte isso
-clear <= not KEY(1);
-set	<= not KEY(2);
+Clock <= not KEY(0); -- os botoes quando nao apertado vale 1                     
+load  <= not KEY(3); -- e apertado 0, essa logica inverte isso
 
-u0 : FlipFlopD port map (
-		clock    => Clock,
-		d        => SW(0),
-		clear    => clear,
-		preset   => set,
-		q        => LEDR(0)
-	);		
- 
+u0 : Ram8 port map (
+		clock   => Clock,
+		input(6 downto 0)   => SW(6 downto 0),
+		input(15 downto 7) => open,
+		load      => load,
+		address   => SW(9 downto 7),
+		output(15 downto 0)  => saida_display
+		
+	);	
+
+     HEX0 <=  "1000000" when to_integer(signed(saida_display)) rem 10 = 0 else --0
+        "1111001" when to_integer(signed(saida_display)) rem 10 = 1 else --1
+        "0100100" when to_integer(signed(saida_display)) rem 10 = 2 else --2
+        "0110000" when to_integer(signed(saida_display)) rem 10 = 3 else --3
+        "0011001" when to_integer(signed(saida_display)) rem 10 = 4 else --4
+        "0010010" when to_integer(signed(saida_display)) rem 10 = 5 else --5
+        "0000010" when to_integer(signed(saida_display)) rem 10 = 6 else --6
+        "1111000" when to_integer(signed(saida_display)) rem 10 = 7 else --7
+        "0000000" when to_integer(signed(saida_display)) rem 10 = 8 else --8
+        "0011000" when to_integer(signed(saida_display)) rem 10 = 9; --9
+        
+  HEX1 <=  "1000000" when to_integer(signed(saida_display)) rem 100 < 10 else --0
+        "1111001" when to_integer(signed(saida_display)) rem 100 < 20 else --1
+        "0100100" when to_integer(signed(saida_display)) rem 100 < 30 else --2
+        "0110000" when to_integer(signed(saida_display)) rem 100 < 40 else --3
+        "0011001" when to_integer(signed(saida_display)) rem 100 < 50 else --4
+        "0010010" when to_integer(signed(saida_display)) rem 100 < 60 else --5
+        "0000010" when to_integer(signed(saida_display)) rem 100 < 70 else --6
+        "1111000" when to_integer(signed(saida_display)) rem 100 < 80 else --7
+        "0000000" when to_integer(signed(saida_display)) rem 100 < 90 else --8
+        "0011000" when to_integer(signed(saida_display)) rem 100 < 100; --9
+        
+  HEX2 <=  "1000000" when to_integer(signed(saida_display)) rem 1000 < 100 else --0
+        "1111001" when to_integer(signed(saida_display)) rem 1000 < 200 else --1
+        "0100100" when to_integer(signed(saida_display)) rem 1000 < 300 else --2
+        "0110000" when to_integer(signed(saida_display)) rem 1000 < 400 else --3
+        "0011001" when to_integer(signed(saida_display)) rem 1000 < 500 else --4
+        "0010010" when to_integer(signed(saida_display)) rem 1000 < 600 else --5
+        "0000010" when to_integer(signed(saida_display)) rem 1000 < 700 else --6
+        "1111000" when to_integer(signed(saida_display)) rem 1000 < 800 else --7
+        "0000000" when to_integer(signed(saida_display)) rem 1000 < 900 else --8
+        "0011000" when to_integer(signed(saida_display)) rem 1000 < 1000; --9
 
 end rtl;
+
--- Projetos/E-LogicaSequencial/src/rtl/Ram4K.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Ram4K.vhd
@@ -20,7 +20,61 @@ architecture arch of Ram4k is
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
 
+	component Ram512 is
+		port(
+			clock:   in  STD_LOGIC;
+			input:   in  STD_LOGIC_VECTOR(15 downto 0);
+			load:    in  STD_LOGIC;
+			address: in  STD_LOGIC_VECTOR( 8 downto 0);
+			output:  out STD_LOGIC_VECTOR(15 downto 0)
+		);
+	end component;
+
+	component Mux8Way16 is
+		port ( 
+				a:   in  STD_LOGIC_VECTOR(15 downto 0);
+				b:   in  STD_LOGIC_VECTOR(15 downto 0);
+				c:   in  STD_LOGIC_VECTOR(15 downto 0);
+				d:   in  STD_LOGIC_VECTOR(15 downto 0);
+				e:   in  STD_LOGIC_VECTOR(15 downto 0);
+				f:   in  STD_LOGIC_VECTOR(15 downto 0);
+				g:   in  STD_LOGIC_VECTOR(15 downto 0);
+				h:   in  STD_LOGIC_VECTOR(15 downto 0);
+				sel: in  STD_LOGIC_VECTOR(2 downto 0);
+				q:   out STD_LOGIC_VECTOR(15 downto 0));
+	end component;
+
+	component DMux8Way is
+		port ( 
+				a:   in  STD_LOGIC;
+				sel: in  STD_LOGIC_VECTOR(2 downto 0);
+				q0:  out STD_LOGIC;
+				q1:  out STD_LOGIC;
+				q2:  out STD_LOGIC;
+				q3:  out STD_LOGIC;
+				q4:  out STD_LOGIC;
+				q5:  out STD_LOGIC;
+				q6:  out STD_LOGIC;
+				q7:  out STD_LOGIC);
+	end component;
+
+	SIGNAL dmux: std_logic_vector(7 downto 0);
+	SIGNAL mux0, mux1, mux2, mux3, mux4, mux5, mux6, mux7: std_logic_vector(15 downto 0);
+
 begin
 
+	DM : DMux8Way port map(a => load, sel => address(11 downto 9), q0 => dmux(0), q1 => dmux(1), q2 => dmux(2), q3 => dmux(3), q4 => dmux(4), q5 => dmux(5), q6 => dmux(6), q7 => dmux(7));
+	
+	r0   : Ram512 port map(clock => clock, input => input, load => dmux(0), address => address(8 downto 0),  output => mux0);
+	r1   : Ram512 port map(clock => clock, input => input, load => dmux(1), address => address(8 downto 0),  output => mux1);
+	r2   : Ram512 port map(clock => clock, input => input, load => dmux(2), address => address(8 downto 0),  output => mux2);
+	r3   : Ram512 port map(clock => clock, input => input, load => dmux(3), address => address(8 downto 0),  output => mux3);
+	r4   : Ram512 port map(clock => clock, input => input, load => dmux(4), address => address(8 downto 0),  output => mux4);
+	r5   : Ram512 port map(clock => clock, input => input, load => dmux(5), address => address(8 downto 0),  output => mux5);
+	r6   : Ram512 port map(clock => clock, input => input, load => dmux(6), address => address(8 downto 0),  output => mux6);
+	r7   : Ram512 port map(clock => clock, input => input, load => dmux(7), address => address(8 downto 0),  output => mux7);
+
+
+	MU  : Mux8Way16 port map(a => mux0, b => mux1, c => mux2, d => mux3, e => mux4, f => mux5, g => mux6, h => mux7, sel => address(11 downto 9), q => output);
 
 end architecture;
